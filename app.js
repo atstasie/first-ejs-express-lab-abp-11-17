241c4daab59e259bb53e68b81c70d1cbda6a5446
Anastassia Troitskaia <atroitskaia@gmail.com>
2017-11-19 18:45:29 +0000

    automatically backed up by learn

--- app.js
+++ app.js
@@ -3,8 +3,19 @@ var app = express()
 
 // Build Your Route Here
 app.get("/", function(req, resp){
-  const viewData = {}
-  resp.render("view file nam", viewData)
+  const welcomeText = "Welcome to HTML"
+  const favoriteThings = ["Cats", "Learning"]
+  const viewData = {
+    "welcomeText": welcomeText,
+    name: "Anastassia",
+    colors: ["Blue", "Red", "Purple"],
+    faoriteThings: favoriteThings
+  }
+
+resp.render("index, viewData")
 })
+
+
+
 
 module.exports = app
