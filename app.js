5a4400d1b89663894230f34b234bcd4a5f248b4f
Anastassia Troitskaia <atroitskaia@gmail.com>
2017-11-19 18:29:03 +0000

    automatically backed up by learn

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ var express = require('express')
 var app = express()
 
 // Build Your Route Here
-app.get("/", function(req, resp))
+app.get("/", function(req, resp){
+  const viewData = {}
+  resp.render("view file nam", viewData)
+})
 
 module.exports = app
